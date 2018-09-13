d39efdc4e59d6e809bf0d0fae3b3557846110552
Victoria A Junaedi <vajunaedi@connect.ust.hk>
2018-09-13 14:19:37 +0800

    I have made a change

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -3,7 +3,9 @@
  */
 public class Library {
 	public static void main(String args[]) {
-		System.out.println("When there is a fire, commit and push.");
+		Library lib = new Library();
+		if(lib.someLibraryMethod())
+			System.out.println("When there is a fire, commit and push.");
 	}
     public boolean someLibraryMethod() {
         return true;
